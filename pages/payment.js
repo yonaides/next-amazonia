8d30d6411ce67180597c86ba0c4fc9aff5e44d3d
Yonaides Tavares <yonaides@gmail.com>
2021-11-03 13:27:25 -0400

    Update payment.js
    cambio de nombre

--- pages/payment.js
+++ pages/payment.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../utils/Store';
 import Layout from '../components/Layout';
-import CheckoutWizard from '../components/CheckoutWizard';
+import CheckoutWizard from '../components/CheckOutWizard';
 import useStyles from '../utils/styles';
 import {
   Button,
