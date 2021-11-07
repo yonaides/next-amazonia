ce7eabe8c10f5e17d01abafac08c221f1e9f0042
yonaides tavares <yonaides@gmail.com>
2021-11-07 11:24:38 -0400

    change db connection properties

--- utils/db.js
+++ utils/db.js
@@ -17,7 +17,10 @@ async function connect() {
     }
     await mongoose.disconnect();
   }
-  const db = await mongoose.connect(process.env.MONGODB_URI, {});
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  });
   console.log("new connection");
   connection.isConnected = db.connections[0].readyState;
 }
